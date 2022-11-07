ff6050b843de405c1d0cd03083ccb0b60953466b
TusharMittal585 <77103048+TusharMittal585@users.noreply.github.com>
2022-11-07 15:08:14 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from operator import ne
+from flask import Flask,jsonify,request
+from data import data
+
+app=Flask(__name__)
+
+app.route("/")
+
+def enter():
+    return "enter planet name"
+
+app.route("/planet")
+
+def planet():
+    name=request.args.get(name)
+    planet_data=next(item for item in data if item["name"]==name)
+
+    return jsonify({
+        "data":planet_data,
+        "message":"success"
+    })
+
+if __name__=="__main__":
+    app.run()
